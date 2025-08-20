33e2301c7344ac049b4a5dcac902e8953a4685ec
Ruslan Araslanov <allmagrave@gmail.com>
2025-08-20 15:56:49 +0300

    add Start

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,3 +1,22 @@
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+    private final int port;
+    private final List<String> validPaths;
+    private final ExecutorService threadPool;
+    private volatile boolean isRunning;
+
+    public Server(int port, List<String> validPaths) {
+        this.port = port;
+        this.validPaths = validPaths;
+        this.threadPool = Executors.newFixedThreadPool(64);
+        this.isRunning = false;
+    }
+
+    public void start() {
+
+    }
 
 }
